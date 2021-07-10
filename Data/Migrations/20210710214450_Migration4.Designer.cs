5f85189814781e4f59eb003c6bb52edd0b3a603b
Matias Gonzalez <matias.gonzalez139@inacapmail.cl>
2021-07-10 17:45:54 -0400

    Migracion4

--- Data/Migrations/20210710214450_Migration4.Designer.cs
+++ Data/Migrations/20210710214450_Migration4.Designer.cs
@@ -4,14 +4,16 @@ using EVA3_MVC_AGENCIA.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EVA3_MVC_AGENCIA.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210710214450_Migration4")]
+    partial class Migration4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20210710214450_Migration4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EVA3_MVC_AGENCIA.Data.Migrations
+{
+    public partial class Migration4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDebt",
+                table: "TReports_clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatePayment",
+                table: "TReports_clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deadline",
+                table: "TReports_clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Credit",
+                table: "TClients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "TClients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDebt",
+                table: "TReports_clients");
+
+            migrationBuilder.DropColumn(
+                name: "DatePayment",
+                table: "TReports_clients");
+
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "TReports_clients");
+
+            migrationBuilder.DropColumn(
+                name: "Credit",
+                table: "TClients");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "TClients");
+        }
+    }
+}
